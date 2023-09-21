485396e0bc045d449e3eef539f3ee409553cbbfd
ismahene.larbi <ismahenelarbi@protonmail.com>
2023-09-21 14:35:27 +0200

    accepting only get requests

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,23 @@
 class HelloWorld
     def call(env)
-        ['200', {"Content-Type" => "text/plain"}, ["Hello World"]]
+        handle_request(env['REQUEST_METHOD'], env['PATH_INFO'])
     end
+
+    private
+    def handle_request(method, path)
+        if method == "GET"
+            get(path)
+        else
+            method_not_allowed(method)
+        end
+    end
+
+    def get(path)
+        [200, {"Content-Type" => "text/plain"}, ["You used a GET request with this path: #{path}"]]
+    end
+
+    def method_not_allowed(method)
+        [405, {}, ["Method not allowed: #{method}"]]
+    end
+
 end
